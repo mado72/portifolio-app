1aff089358db3530c8dda26b9c4b5a1412a4cf23
Marcelo Damasceno <marcelo.damasceno@globality.com.br>
2025-03-11 02:52:24 -0300

    feat: portfolio + investment-portfolio-container

--- src/app/dashboard/portfolio/portfolio.component.ts
+++ src/app/dashboard/portfolio/portfolio.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AllocationSummaryComponent } from '../../assets/allocation-summary/allocation-summary.component';
 import { BalancesComponent } from '../../assets/balances/balances.component';
 import { FinancialForecastSummaryComponent } from '../../assets/financial-forecast-summary/financial-forecast-summary.component';
+import { InvestmentPortfolioContainerComponent } from "../../investment/investment-portfolio-container/investment-portfolio-container.component";
 
 @Component({
   selector: 'app-portfolio',
@@ -9,8 +10,9 @@ import { FinancialForecastSummaryComponent } from '../../assets/financial-foreca
   imports: [
     AllocationSummaryComponent,
     BalancesComponent,
-    FinancialForecastSummaryComponent
-  ],
+    FinancialForecastSummaryComponent,
+    InvestmentPortfolioContainerComponent
+],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
