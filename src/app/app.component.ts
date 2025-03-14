8d52bdf6463a81cbdc692638d247ccafdd2ba185
Marcelo Damasceno <marcelo.damasceno@globality.com.br>
2025-03-14 12:27:03 -0300

    style: Ajuste no layout do container principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MenuComponent } from './layout/menu/menu.component';
 import { HeaderComponent } from "./layout/header/header.component";
 
 @Component({
@@ -8,7 +7,6 @@ import { HeaderComponent } from "./layout/header/header.component";
   standalone: true,
   imports: [
     RouterOutlet,
-    MenuComponent,
     HeaderComponent
 ],
   templateUrl: './app.component.html',
