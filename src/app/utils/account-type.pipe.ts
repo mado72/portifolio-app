ab7a6727e1d02ed05ca37db241464e5b9895881b
Marcelo Damasceno <marcelo.damasceno@globality.com.br>
2025-03-21 13:19:45 -0300

    feat: account-type pipe

---
+++ src/app/utils/account-type.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { AccountTypeEnum } from '../model/domain.model';
+
+const AccountTypeDesc : Record<AccountTypeEnum, string> = {
+  CHECKING: 'Conta Corrente',
+  SAVINGS: 'Conta Poupança',
+  INVESTMENT: 'Conta Investimento',
+  LOAN: 'Empréstimo',
+  OTHER: 'Outro'
+}
+@Pipe({
+  name: 'accountType',
+  standalone: true
+})
+export class AccountTypePipe implements PipeTransform {
+
+  transform(value: AccountTypeEnum): string {
+    return AccountTypeDesc[value];
+  }
+
+}
