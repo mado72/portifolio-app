7a1a3997a85412d0c1754a7891b1c7b30954ff88
Marcelo Damasceno <marcelo.damasceno@globality.com.br>
2025-03-23 16:45:58 -0300

    feat: TransactionTable uses TransactionDialog

--- src/app/transaction/transaction-dialog/transaction-dialog.component.ts
+++ src/app/transaction/transaction-dialog/transaction-dialog.component.ts
@@ -1,15 +1,158 @@
-import { Component } from '@angular/core';
-import { MatDialogModule } from '@angular/material/dialog';
+import { Component, computed, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { TransactionTypePipe } from '../transaction-type.pipe';
+import { TransactionStatusPipe } from '../transaction-status.pipe';
+import { Asset, TransactionEnum, TransactionStatus, TransactionType } from '../../model/investment.model';
+import { Currency } from '../../model/domain.model';
+import { JsonPipe, KeyValuePipe } from '@angular/common';
+import { provideAppDateAdapter } from '../../utils/app-date-adapter.adapter';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { InvestmentService } from '../../service/investment.service';
+import { getMarketPlaceCode } from '../../service/quote.service';
+
+export type TransactionDialogType = {
+  title: string,
+  newTransaction: boolean,
+  transaction: TransactionType
+};
 
 @Component({
   selector: 'app-transaction-dialog',
   standalone: true,
   imports: [
-    MatDialogModule
+    ReactiveFormsModule,
+    KeyValuePipe,
+    JsonPipe,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    TransactionTypePipe,
+    TransactionStatusPipe
+  ],
+  providers: [
+    provideNativeDateAdapter()
   ],
   templateUrl: './transaction-dialog.component.html',
   styleUrl: './transaction-dialog.component.scss'
 })
-export class TransactionDialogComponent {
+export class TransactionDialogComponent implements OnInit {
+
+  private dialogRef = inject(MatDialogRef<TransactionDialogComponent>);
+  
+  readonly data = inject<TransactionDialogType>(MAT_DIALOG_DATA);
+  
+  private investmentService = inject(InvestmentService);  
+  
+  private fb = inject(FormBuilder);  
+
+  readonly transactionForm = this.fb.group({
+    id: this.fb.control(this.data.transaction.id),
+    ticket: this.fb.control(this.data.transaction.ticket),
+    date: this.fb.control(this.data.transaction.date, []),
+    accountId: this.fb.control(this.data.transaction.accountId, []),
+    quantity: this.fb.control(this.data.transaction.quantity, []),
+    quote: this.fb.control(this.data.transaction.quote, []),
+    value: this.fb.group({
+      amount: this.fb.control(this.data.transaction.value.amount, []),
+      currency: this.fb.control(this.data.transaction.value.currency, [])
+    }),
+    type: this.fb.control(this.data.transaction.type, []),
+    status: this.fb.control(this.data.transaction.status, []),
+    brokerage: this.fb.control(this.data.transaction.brokerage, [])
+  });
+
+  readonly transactionTypes = Object.values(TransactionEnum);
+  readonly transactionStatuses = Object.values(TransactionStatus);
+  readonly currencies = Object.values(Currency);
+
+  
+  assets = computed(() => {
+    if (!this.data.transaction.ticket) {
+      return Object.values(this.investmentService.assertsSignal()).reduce((acc, asset)=>{
+        acc[getMarketPlaceCode(asset)] = asset;
+        return acc;
+      }, {} as {[key: string]:Asset});
+    }
+    return undefined;
+  })
+
+  ngOnInit(): void {
+    if (!this.data.newTransaction) {
+      this.ticket.disable();
+    }
+  }
+
+  get ticket() {
+    return this.transactionForm.get('ticket') as FormControl;
+  }
+
+  get date() {
+    return this.transactionForm.get('date') as FormControl;
+  }
+
+  get accountId() {
+    return this.transactionForm.get('accountId') as FormControl;
+  }
+
+  get quantity() {
+    return this.transactionForm.get('quantity') as FormControl;
+  }
+
+  get quote() {
+    return this.transactionForm.get('quote') as FormControl;
+  }
+
+  get amount() {
+    return this.transactionForm.get('value.amount') as FormControl;
+  }
+
+  get currency() {
+    return this.transactionForm.get('value.currency') as FormControl;
+  }
+
+  get type() {
+    return this.transactionForm.get('type') as FormControl;
+  }
+
+  get status() {
+    return this.transactionForm.get('status') as FormControl;
+  }
+
+  get brokerage() {
+    return this.transactionForm.get('brokerage') as FormControl;
+  }
+
+  get assetSelected() {
+    const assets = this.assets();
+    if (this.ticket.value && assets) {
+      return assets[this.ticket.value];
+    }
+    return undefined ;
+  }
+
+  submitForm() {
+    this.dialogRef.close(this.transactionForm.value);
+  }
+
+  quantityQuoteUpdated() {
+    this.amount.setValue(Math.trunc(100 * this.quantity.value * this.quote.value) / 100 || undefined)
+  }
+
+  amountUpdated() {
+    this.quote.setValue(Math.trunc(100 * this.amount.value / this.quantity.value) / 100 || undefined)
+  }
+
+  selectAll($event: FocusEvent) {
+    ($event.target as HTMLInputElement).select();
+  }
 
 }
--- src/app/transaction/transaction-table/transaction-table.component.ts
+++ src/app/transaction/transaction-table/transaction-table.component.ts
@@ -1,13 +1,17 @@
-import { Component, inject } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
-import { TransactionType } from '../../model/investment.model';
 import { DatePipe, DecimalPipe } from '@angular/common';
-import { CurrencyComponent } from '../../utils/currency/currency.component';
-import { TransactionTypePipe } from '../transaction-type.pipe';
-import { TransactionStatusPipe } from '../transaction-status.pipe';
-import { MatIconModule } from '@angular/material/icon';
-import { TransactionService } from '../../service/transaction.service';
+import { ChangeDetectorRef, Component, effect, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { Currency } from '../../model/domain.model';
+import { TransactionEnum, TransactionStatus, TransactionType } from '../../model/investment.model';
+import { TransactionService } from '../../service/transaction.service';
+import { CurrencyComponent } from '../../utils/currency/currency.component';
+import { TransactionDialogComponent, TransactionDialogType } from '../transaction-dialog/transaction-dialog.component';
+import { TransactionStatusPipe } from '../transaction-status.pipe';
+import { TransactionTypePipe } from '../transaction-type.pipe';
+import { isAfter, isBefore } from 'date-fns';
 
 @Component({
   selector: 'app-transaction-table',
@@ -26,27 +30,79 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './transaction-table.component.scss'
 })
 export class TransactionTableComponent {
-addTransaction() {
-throw new Error('Method not implemented.');
-}
 
   private transactionService = inject(TransactionService); // Assuming this is a service that provides transaction data
 
+  private dialog = inject(MatDialog);
+
+  private changeDetectorRef = inject(ChangeDetectorRef);
+
   readonly displayedColumns = ["ticket", "date", "type", "quote", "value", "status", "account", "brokerage", "actions"];
 
-  dataSource = this.transactionService.getTransactions();
+  // tData = this.transactionService.transactionsData();
+
+  dataSource: TransactionType[] = [];
+
+  constructor() {
+    effect(()=>{
+      this.dataSource = Object.values(this.transactionService.transactionsData()).slice().sort((a, b) => isAfter(a.date, b.date) ? -1: isBefore(a.date, b.date) ? 1 : 0);
+      console.log(`Datasource changed:`, this.dataSource);
+    })
+
+  }
 
   getAccount(accountId: string) {
     // Simulate fetching account details
     return `Account ${accountId}`;
   }
 
-  deleteTransaction(_t133: any) {
-    throw new Error('Method not implemented.');
+  addTransaction() {
+    this.openDialog({
+      newTransaction: true,
+      title: 'Adicionar Transação',
+      transaction: {
+        ticket: '',
+        date: new Date(),
+        accountId: '',
+        quantity: 1,
+        quote: 1,
+        value: { amount: 0, currency: Currency.BRL },
+        type: TransactionEnum.BUY,
+        status: TransactionStatus.COMPLETED
+      }
+    })
   }
 
-  editTransaction(_t133: any) {
-    throw new Error('Method not implemented.');
+  deleteTransaction(event: MouseEvent, transaction: TransactionType) {
+    event.stopPropagation();
+    this.transactionService.deleteTransaction(transaction.id as string).subscribe(_=>{
+      this.changeDetectorRef.detectChanges(); // Refresh table data
+    });
   }
+
+  editTransaction(transaction: TransactionType) {
+    this.openDialog({
+      newTransaction: false,
+      title: 'Editar Transação',
+      transaction,
+    })
+  }
+
+  openDialog(data: TransactionDialogType) {
+    const dialogRef = this.dialog.open(TransactionDialogComponent, {
+      data
+    })
+
+    dialogRef.afterClosed().subscribe((result: TransactionType) => {
+      if (result) {
+        this.transactionService.saveTransaction({
+          ...data.transaction, ...result
+        }).subscribe(_ => {
+          this.changeDetectorRef.detectChanges(); // Refresh table data
+        });
+      }
+    });
+  }
+
 
 }
