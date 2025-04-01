cbebcc79fd7cf3dc3388bfc41f60f0d61bcaea11
Marcelo Damasceno <marcelo.damasceno@globality.com.br>
2025-04-01 03:14:33 -0300

    feat: environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    apiBaseUrl: 'http://localhost:8080',  // Replace with your actual API URL
+    marketPlace: {
+        BVMF: {
+            apiUrl: 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={code}.SA&apikey=KH1AWC9WXP5WBUQ2'
+        }
+    }
+}
