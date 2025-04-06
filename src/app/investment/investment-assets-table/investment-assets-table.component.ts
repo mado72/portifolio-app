d5791298e2f55c1e561cd1b35ab7a978e5e13e93
Marcelo Damasceno <marcelo.damasceno@globality.com.br>
2025-04-06 03:26:27 -0300

    feat: add, edit and delete asset from asset table

--- src/app/investment/investment-assets-table/investment-assets-table.component.ts
+++ src/app/investment/investment-assets-table/investment-assets-table.component.ts
@@ -1,47 +1,81 @@
-import { DatePipe } from '@angular/common';
-import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { DatePipe, JsonPipe } from '@angular/common';
+import { Component, computed, inject, input, OnInit, TrackByFunction } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCircleCheck, faXmarkCircle } from '@fortawesome/free-solid-svg-icons';
+import { AssetQuoteType } from '../../model/source.model';
+import { AssetService } from '../../service/asset.service';
 import { InvestmentService } from '../../service/investment.service';
+import { PortfolioService } from '../../service/portfolio-service';
 import { AssetCodePipe } from '../../utils/asset-code.pipe';
 import { AssetTypePipe } from '../../utils/asset-type.pipe';
 import { CurrencyComponent } from '../../utils/currency/currency.component';
 import { TrendComponent } from '../../utils/trend/trend.component';
-import { AssetQuoteType } from '../../model/source.model';
 
 @Component({
   selector: 'app-investment-assets-table',
   standalone: true,
   imports: [
     MatTableModule,
+    MatIconModule,
+    MatButtonModule,
     DatePipe,
     FontAwesomeModule,
     CurrencyComponent,
     TrendComponent,
     AssetTypePipe,
-    AssetCodePipe
+    AssetCodePipe,
+    JsonPipe
   ],
   templateUrl: './investment-assets-table.component.html',
   styleUrl: './investment-assets-table.component.scss'
 })
-export class InvestmentAssetsTableComponent {
+export class InvestmentAssetsTableComponent implements OnInit {
 
-  private investmentService = inject(InvestmentService);
+  private assetService = inject(AssetService);
+
+  private investimentService = inject(InvestmentService);
+
+  private portfolioService = inject(PortfolioService);
+
+  editable = input<boolean>(false);
+
+  datasource = computed(() => {
+    const assets = Object.values(this.investimentService.assertsSignal())
+      .map(asset => ({
+        ...asset,
+        deletable: this.editable() && (this.portfolioService.portfoliosOfAsset(asset)?.length || 0) < 1
+      }))
+    return assets;
+  });
 
   readonly iconTrue = faCircleCheck;
+
   readonly iconFalse = faXmarkCircle;
 
   readonly displayedColumns: string[] = ['name', 'code', 'type', 'quote', 'trend', 'lastUpdate', 'controlByQty', 'manualQuote'];
 
-  @Input() enableSelection: boolean = false;
+  ngOnInit() {
+    if (this.editable()) {
+      this.displayedColumns.push('actions');
+    }
+  }
 
-  @Output() onSelected = new EventEmitter<AssetQuoteType>();
+  trackByFn: TrackByFunction<AssetQuoteType> = (index: number, item: AssetQuoteType) => {
+    return item.ticker;
+  };
+  editAsset(asset: AssetQuoteType) {
+    this.assetService.editDialog(asset);
+  }
 
-  @Input() datasource!: (AssetQuoteType)[];
+  newAsset() {
+    this.assetService.newDialog();
+  }
 
-  rowClick(asset: AssetQuoteType) {
-    this.onSelected.emit(asset);
+  deleteAsset(asset: AssetQuoteType) {
+    this.assetService.deleteAsset(asset.ticker);
   }
   
 }
