c974678efe8a1cfd8cc60d5c67557f211efe6aa8
Marcelo Damasceno <marcelo.damasceno@globality.com.br>
2025-05-05 14:27:57 -0300

    feat: Integrate PortfolioEvolutionChart component into profitability view

--- src/app/profitability/profitability/profitability.component.ts
+++ src/app/profitability/profitability/profitability.component.ts
@@ -2,13 +2,15 @@ import { Component, computed, inject, signal } from '@angular/core';
 import { ProfitabilityService } from '../../service/profitalibilty.service';
 import { CellChangeEvent, GridData } from '../../utils/component/financial-grid/financial-gird.model';
 import { FinancialGridComponent } from '../../utils/component/financial-grid/financial-grid.component';
+import { PortfolioEvolutionChartComponent } from '../../components/portfolio-evolution-chart/portfolio-evolution-chart.component';
 import { getYear } from 'date-fns';
 
 @Component({
   selector: 'app-profitability',
   standalone: true,
   imports: [
-    FinancialGridComponent
+    FinancialGridComponent,
+    PortfolioEvolutionChartComponent // Importa o novo componente
   ],
   templateUrl: './profitability.component.html',
   styleUrl: './profitability.component.scss'
@@ -42,4 +44,12 @@ export class ProfitabilityComponent {
     const value = event.value as number || 0;
     this.profitabilityService.updateFinancialGridData(event);
   }
+
+  // Dados simulados para o gráfico
+  portfolioEvolutionData = [
+    { name: '2021', value: 50000 },
+    { name: '2022', value: 75000 },
+    { name: '2023', value: 100000 },
+    { name: '2024', value: 125000 }
+  ];
 }
